60a765d69abe5a49888a660ec8fc792b6a2b4483
Birkan <birkanyksl613@gmail.com>
2023-12-05 06:41:41 +0300

    Bankapp Website

--- script.js
+++ script.js
@@ -5,8 +5,20 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+
+const section1 = document.querySelector("#section--1");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector(".nav");
+
+//////////////////////////////////////////
 
 const openModal = function (e) {
   e.preventDefault();
@@ -33,3 +45,83 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Page Navigation
+
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// Smooth scrolling
+
+btnScrollTo.addEventListener("click", function (e) {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+// Tabbed Component
+
+// tabs.forEach(function (t) {
+//   t.addEventListener("click", function () {
+//     console.log("tab");
+//   });
+// });
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard Class
+  if (!clicked) return;
+  tabs.forEach(function (t) {
+    t.classList.remove("operations__tab--active");
+  });
+  clicked.classList.add("operations__tab--active");
+
+  tabsContent.forEach(function (c) {
+    c.classList.remove("operations__content--active");
+  });
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// Menu Fade Animations
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+// Passing "argument" into handler
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+//Sticky Navigation Scroll Event
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+// Sticky Navigation: Intersection Observer API

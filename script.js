0c8edc8578e32d6381930ead61a1cc1e26751e83
Birkan <birkanyksl613@gmail.com>
2023-12-06 01:34:26 +0300

    bankappwebsite

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  //console.log(entry);
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
